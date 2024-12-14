3c429cd9ce3f911f1bef5f976e2ac42bcbc419ea
Manami Kokado <kokado313.mnm@gmail.com>
2024-12-14 20:39:16 +0900

    24/12/14 範囲

--- src/Game.kt
+++ src/Game.kt
@@ -6,25 +6,24 @@ fun main() {
 
     // Aura
     val auraVisible = isBlessed && healthPoints > 50 || isImmortal
-    if (auraVisible) {
-        println("GREEN")
+    val auraColor = if (auraVisible) "GREEN" else "NONE"
+    println(auraColor)
+
+    val healthStatus = if (healthPoints == 100) {
+        "is in excellent condition!"
+    } else if (healthPoints in 90..99) {
+        "has a few scratches."
+    } else if (healthPoints in 75 .. 89) {
+        if (isBlessed) {
+            "has some minor wounds but is healing quite quickly!"
+        } else {
+            "has some minor wounds."
+        }
+    } else if (healthPoints in 15..74) {
+        "looks pretty hurt."
     } else {
-        println("NONE")
+        "is in awful condition!"
     }
 
-    if (healthPoints == 100) {
-        println("$name is in excellent condition!")
-    } else if (healthPoints >= 90) {
-        println("$name has a few scratches.")
-    } else if (healthPoints >= 75) {
-        if (isBlessed) {
-            println("$name has some minor wounds but is healing quite quickly!")
-        } else {
-            println("$name has some minor wounds.")
-        }
-    } else if (healthPoints >= 15) {
-        println("$name looks pretty hurt.")
-    } else {
-        println("$name is in awful condition!")
-    }
+    println("$name $healthStatus")
 }
