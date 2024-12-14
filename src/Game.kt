922087a2266b1cd43fbdcda50caa7f1b83a6ca66
Manami Kokado <kokado313.mnm@gmail.com>
2024-12-14 20:52:24 +0900

    24/12/14 when式

--- src/Game.kt
+++ src/Game.kt
@@ -9,21 +9,29 @@ fun main() {
     val auraColor = if (auraVisible) "GREEN" else "NONE"
     println(auraColor)
 
-    val healthStatus = if (healthPoints == 100) {
-        "is in excellent condition!"
-    } else if (healthPoints in 90..99) {
-        "has a few scratches."
-    } else if (healthPoints in 75 .. 89) {
-        if (isBlessed) {
+    // Health Status
+    val healthStatus = when (healthPoints) {
+        100 -> "is in excellent condition!"
+        in 90..99 -> "has a few scratches."
+        in 75..89 -> if (isBlessed) {
             "has some minor wounds but is healing quite quickly!"
         } else {
             "has some minor wounds."
         }
-    } else if (healthPoints in 15..74) {
-        "looks pretty hurt."
-    } else {
-        "is in awful condition!"
+        in 15..74 -> "looks pretty hurt."
+        else -> "is in awful condition!"
     }
 
+
     println("$name $healthStatus")
+
+//    val race = "gnome"
+//    val faction = when (race) {
+//        "dwarf" -> "Keepers of the Mines"
+//        "gnome" -> "Keepers of the Mines"
+//        "orc" -> "Free People of the Rolling Hills"
+//        "human" -> "Free People of the Rolling Hills"
+//    }
+//
+//    println(faction)
 }
