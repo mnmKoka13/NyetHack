a002e05559c7a62996b78b319a190a4275f68876
Manami Kokado <kokado313.mnm@gmail.com>
2024-12-22 23:03:08 +0900

    24/12/22 null安全：二重感嘆符演算子

--- src/Tavern.kt
+++ src/Tavern.kt
@@ -1,14 +1,6 @@
 fun main(args: Array<String>) {
-    // セーフコール演算子
-//    var beverage = readLine()?.capitalize()
-    // セーフコール演算子とlet関数
-    var beverage = readLine()?.let {
-        if (it.isNotBlank()) {
-            it.capitalize()
-        } else {
-            "Buttered Ale"
-        }
-    }
+    // 二重感嘆符演算子
+    var beverage = readLine()!!.capitalize()
 
 //    beverage = null
     println(beverage)
