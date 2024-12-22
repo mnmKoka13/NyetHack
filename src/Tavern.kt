fbcc98e3048c1d7d061a6d4cbac52a67966afc30
Manami Kokado <kokado313.mnm@gmail.com>
2024-12-22 22:58:53 +0900

    24/12/22 null安全：セーフコール演算子とlet

--- src/Tavern.kt
+++ src/Tavern.kt
@@ -1,5 +1,15 @@
 fun main(args: Array<String>) {
-    var beverage = readLine()
+    // セーフコール演算子
+//    var beverage = readLine()?.capitalize()
+    // セーフコール演算子とlet関数
+    var beverage = readLine()?.let {
+        if (it.isNotBlank()) {
+            it.capitalize()
+        } else {
+            "Buttered Ale"
+        }
+    }
+
 //    beverage = null
     println(beverage)
 }
