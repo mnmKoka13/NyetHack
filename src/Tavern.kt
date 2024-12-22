30319af656294f608e82eae721c47af8b7ed9536
Manami Kokado <kokado313.mnm@gmail.com>
2024-12-22 23:06:31 +0900

    24/12/22 null安全：if文でチェックする

--- src/Tavern.kt
+++ src/Tavern.kt
@@ -1,7 +1,13 @@
 fun main(args: Array<String>) {
     // 二重感嘆符演算子
-    var beverage = readLine()!!.capitalize()
+    var beverage = readLine()
 
 //    beverage = null
+
+    if (beverage != null) {
+        beverage = beverage.capitalize()
+    } else {
+        println("I can't do that without crashing -beverage was null!")
+    }
     println(beverage)
 }
