158b055d066bb02cbf50984d800831e858acd9b9
Manami Kokado <kokado313.mnm@gmail.com>
2025-02-01 16:57:43 +0900

    25/2/1 クラス定義１

--- src/Game.kt
+++ src/Game.kt
@@ -1,58 +1,21 @@
 fun main() {
-    val name = "Madrigal"
-    var healthPoints = 89
-    val isBlessed = true
-    val isImmortal = false
+    // Player
+    val player = Player()
+    player.castFireball()
 
     // Aura
-    val auraColor = getAuraColor(isBlessed, healthPoints, isImmortal)
-
-    // Health Status
-    val healthStatus = formatHealthStatus(healthPoints, isBlessed)
+    val auraColor = player.auraColor()
 
     // Player Status
-    printPlayerStatus(auraColor, isBlessed, name, healthStatus)
-
-    // Cast Fireball
-    castFireBall(5)
-    castFireBall() // Default value
+    printPlayerStatus(player)
 
 }
 
-private fun printPlayerStatus(
-    auraColor: String,
-    isBlessed: Boolean,
-    name: String,
-    healthStatus: String
-) {
+private fun printPlayerStatus(player: Player) {
     println(
-        "(Aura: $auraColor) " +
-                "(Blessed: ${if (isBlessed) "YES" else "NO"})"
+        "(Aura: ${player.auraColor()}) " +
+                "(Blessed: ${if (player.isBlessed) "YES" else "NO"})"
     )
-    println("$name $healthStatus")
+    println("${player.name} ${player.formatHealthStatus()}")
 }
 
-private fun getAuraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean): String {
-    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
-    val auraColor = if (auraVisible) "GREEN" else "NONE"
-    return auraColor
-}
-
-// 単一式関数に変更
-private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean) =
-    when (healthPoints) {
-        100 -> "is in excellent condition!"
-        in 90..99 -> "has a few scratches."
-        in 75..89 -> if (isBlessed) {
-            "has some minor wounds but is healing quite quickly!"
-        } else {
-            "has some minor wounds."
-        }
-
-        in 15..74 -> "looks pretty hurt."
-        else -> "is in awful condition!"
-    }
-
-// 単一式関数に変更
-private fun castFireBall(numFireballs: Int = 2) =
-    println("A glass of Fireball springs into existence. (x$numFireballs)")
