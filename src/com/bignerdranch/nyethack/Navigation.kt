dc1402958b41e44414a4363c5c7539afb59b10e0
Manami Kokado <kokado313.mnm@gmail.com>
2025-02-22 16:07:57 +0900

    25/02/22 オブジェクト-演算子の多重定義

--- src/com/bignerdranch/nyethack/Navigation.kt
+++ src/com/bignerdranch/nyethack/Navigation.kt
@@ -6,12 +6,15 @@ enum class Direction(private val coordinate: Coordinate) {
     SOUTH(Coordinate(0, 1)),
     WEST(Coordinate(-1, 0));
 
-    fun updateCoordinate(playerCoordinate: Coordinate) {
-        Coordinate(playerCoordinate.x + coordinate.x,
-                playerCoordinate.y + coordinate.y)
-    }
+    fun updateCoordinate(playerCoordinate: Coordinate) =
+        coordinate + playerCoordinate
 }
 
 data class Coordinate(val x: Int, val y: Int) {
     val isInBounds = x >= 0 && y >= 0
+
+    // 演算子の多重定義
+    // その他よく使われる多重定義可能な演算子
+    // +=: plusAssign, ==: equals, >: compareTo, []: get, ..: rangeTo, in: contains
+    operator fun plus(other: Coordinate) = Coordinate(x + other.x, y + other.y)
 }
