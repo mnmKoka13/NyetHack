53adf31f27c0b2317e6bb73ca299c9d0f2e56605
Manami Kokado <kokado313.mnm@gmail.com>
2025-02-22 14:45:04 +0900

    25/02/22 オブジェクト-objectキーワード

--- src/com/bignerdranch/nyethack/Game.kt
+++ src/com/bignerdranch/nyethack/Game.kt
@@ -4,26 +4,40 @@ import Room
 import TownSquare
 
 fun main() {
-    // com.bignerdranch.nyethack.Player
+    Game.play()
+}
+
+
+// objectキーワード：シングルトンオブジェクト
+//　最初にアクセスされた時、そのクラスのインスタンスがただ１つ生成される
+// プログラム実行中は常に同じインスタンスを返す
+object Game {
     val player = Player("Madrigal")
-    player.castFireball()
-
-    // Aura
-    val auraColor = player.auraColor()
-
     var currentRoom: Room = TownSquare()
-    println(currentRoom.description())
-    println(currentRoom.load())
 
-    // com.bignerdranch.nyethack.Player Status
-    printPlayerStatus(player)
+    init {
+        println("Welcome, adventure.")
+        player.castFireball()
+    }
+
+    fun play() {
+        while (true) {
+            // Play NyetHack
+            println(currentRoom.description())
+            println(currentRoom.load())
+
+            printPlayerStatus(player)
+
+            print("> Enter your command: ")
+            println("Last command: ${readLine()}")
+        }
+    }
+
+    private fun printPlayerStatus(player: Player) {
+        println(
+            "(Aura: ${player.auraColor()}) " +
+                    "(Blessed: ${if (player.isBlessed) "YES" else "NO"})"
+        )
+        println("${player.name} ${player.formatHealthStatus()}")
+    }
 }
-
-private fun printPlayerStatus(player: Player) {
-    println(
-        "(Aura: ${player.auraColor()}) " +
-                "(Blessed: ${if (player.isBlessed) "YES" else "NO"})"
-    )
-    println("${player.name} ${player.formatHealthStatus()}")
-}
-
